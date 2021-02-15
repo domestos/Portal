a6673cc891a3a1a051cf8941a67beb47d2514616
Valeriy Pelenskyi <vap@csia.net>
2021-02-15 20:27:12 +0200

    edit consumers.py

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -8,14 +8,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
+        self.username = self.scope['user']
+              
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
+        await self.channel_layer.group_send(
+            self.room_group_name, {
+                'type':'tester_message',
+                'tester':'tester'
+
+            }
+        )
+
         await self.accept()
+
+    async def tester_message(self, event):
+        tester = event['tester']
+        
+        await self.send(text_data=json.dumps({
+            'tester':tester,
+        }))
 
     async def disconnect(self, close_code):
         # Leave room group
--- src/config/settings.py
+++ src/config/settings.py
@@ -62,15 +62,20 @@ ROOT_URLCONF = 'config.urls'
 
 ASGI_APPLICATION = "config.asgi.application"
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
